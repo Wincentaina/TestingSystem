bacf1cf29ab65a8b224e10d78882e7f22b86ad78
Wincentaina <103582986+Wincentaina@users.noreply.github.com>
2024-12-30 18:43:29 +0700

    init

---
+++ src/test/java/ru/wincentaina/TestingSystem/TestingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wincentaina.TestingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
